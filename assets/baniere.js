fa9f575a7bedaa3795e5c3691244426d840302a7
iness0509 <iness.sakal@gmail.com>
2026-01-27 20:27:34 +0100

    new file:   assets/baniere.js 	modified:   assets/style.css 	modified:   index.html 	deleted:    assets/script.js

---
+++ assets/baniere.js
@@ -0,0 +1,45 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+let position = 0
+
+
+const bannerImage = document.querySelector('.banner-img');
+const tagLine = document.querySelector('#banner p');
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const dots = document.querySelectorAll('.dot'); 
+
+function  updateSlide(position) { 
+    bannerImage.src = `./assets/images/slideshow/${slides[position].image}`; 
+    tagLine.innerHTML = slides[position].tagLine; 
+    dots.forEach((dot, i) =>
+    dot.classList.toggle("dot_selected", i === position) 
+);}
+
+arrowRight.addEventListener('click', function () {
+    position = (position + 1) % slides.length;
+    updateSlide(position);   
+})
+
+arrowLeft.addEventListener ('click', function() {
+    position = (position - 1 + slides.length) % slides.length;
+    updateSlide(position);
+})
+
